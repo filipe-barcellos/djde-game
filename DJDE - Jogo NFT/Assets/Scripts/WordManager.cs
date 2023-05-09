818d5cb381581c59f05750746fc9178ea9418b72
Valenco Silva <101263360+ValencoSilva@users.noreply.github.com>
2023-05-09 18:07:28 -0300

    Att sistema de derrora e vitoria

--- DJDE - Jogo NFT/Assets/Scripts/WordManager.cs
+++ DJDE - Jogo NFT/Assets/Scripts/WordManager.cs
@@ -14,6 +14,7 @@ public class Word
 public class WordManager : MonoBehaviour
 {
     public string[] wordList;
+    public int tentativa;
     public string chosenWord;
     public Word[] words;
     public Color wrongColor, rightColor, yellow;
@@ -51,48 +52,67 @@ public class WordManager : MonoBehaviour
     }
 
 
-    public void Enter()
+  public void Enter()
+{
+    if (letterIndex <= 4)
+        return;
+    
+    string newWord = chosenWord;
+    char[] newWordArray = newWord.ToCharArray();
+
+    List<int> rightLetters = new List<int>();
+    for(int i = 0; i < 5; i++)
     {
-        if (letterIndex <= 4)
-            return;
-        
-
-        string newWord = chosenWord;
-        char[] newWordArray = newWord.ToCharArray();
-
-        List<int> rightLetters = new List<int>();
-        for(int i = 0; i < 5; i++)
+        if(words[wordIndex].letters[i].text==chosenWord[i].ToString())
         {
-            if(words[wordIndex].letters[i].text==chosenWord[i].ToString())
-            {
-                words[wordIndex].letterBg[i].color = rightColor;
-                newWordArray[i] = ' ';
-                rightLetters.Add(i);
-                SetKeyColor(words[wordIndex].letters[i].text,rightColor,true);
-            }
+            words[wordIndex].letterBg[i].color = rightColor;
+            newWordArray[i] = ' ';
+            rightLetters.Add(i);
+            SetKeyColor(words[wordIndex].letters[i].text,rightColor,true);
         }
-
-        newWord = new string(newWordArray);
-        for(int i = 0; i < 5; i++)
-        {
-            if(!rightLetters.Contains(i))
-            {
-                if(newWord.Contains(words[wordIndex].letters[i].text))
-                {
-                    words[wordIndex].letterBg[i].color = yellow;
-                    SetKeyColor(words[wordIndex].letters[i].text,yellow);
-                }
-                else
-                {
-                    words[wordIndex].letterBg[i].color = wrongColor;
-                    SetKeyColor(words[wordIndex].letters[i].text,wrongColor);
-                }
-            }
-        }
-
-        wordIndex++;
-        letterIndex = 0;
     }
+
+    newWord = new string(newWordArray);
+    bool wordIsCorrect = true;
+    for(int i = 0; i < 5; i++)
+    {
+        if(!rightLetters.Contains(i))
+        {
+            if(newWord.Contains(words[wordIndex].letters[i].text))
+            {
+                words[wordIndex].letterBg[i].color = yellow;
+                SetKeyColor(words[wordIndex].letters[i].text,yellow);
+                wordIsCorrect = false;
+            }
+            else
+            {
+                words[wordIndex].letterBg[i].color = wrongColor;
+                SetKeyColor(words[wordIndex].letters[i].text,wrongColor);
+                wordIsCorrect = false;
+            }
+        }
+    }
+
+    if (wordIsCorrect)
+    {
+        Debug.Log("Você acertou a palavra!");
+    }
+    else
+    {
+        tentativa +=1;
+        Debug.Log("tentativa numero:"+tentativa+"de 6");
+        Debug.Log("Você errou a palavra!");
+        if(tentativa ==6){
+            Debug.Log("Perdeu!");
+            
+        }
+    }
+
+    wordIndex++;
+    letterIndex = 0;
+}
+
+
 
     void SetKeyColor(string letter,Color color, bool correct = false)
     {
